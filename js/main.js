8f0446f9b1d3e721e44c0388c9ccb04ffa8a7976
Himanshu Machra <himanshumachra25@gmail.com>
2026-09-15 17:02:28 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// JavaScript for the workshop landing page
+
+document.addEventListener('DOMContentLoaded', () => {
+    // We will add interactive logic here for things like the FAQ accordion, Countdown Timer, etc.
+    console.log("CountryEdu Landing Page Initialized!");
+});
---
+++ p
@@ -0,0 +1,1 @@
+deploy it to the github https://github.com/ytpremium78246-cell/03octworkshop
